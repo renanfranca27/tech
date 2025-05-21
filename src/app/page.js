4811f8c55375192b946887b0692d2b5f769c177d
renanfranca27 <renanfrancabrito27@gmail.com>
2025-05-21 01:18:49 -0300

    modificação no main

--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,49 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Main from "@/components/main/Main.js";
 
 export default function Home() {
   return (
-
     <div className={styles.container}>
       {/* Menu com estrutura correta */}
       <div className={styles.menu}>
+        <section className={styles.navbar}>
+          <Image src="/icone.svg" alt="Descrição da imagem" width={90} height={60} />
+          TECH SOLUTIONS
+        </section>
+        <ul className={styles.menubar}>
+          <li>Inicio</li>
+          <li>Serviços</li>
+          <li>Sobre</li>
+          <li>Contato</li>
+        </ul>
+      </div>
 
-          <section className={styles.navbar}>
-            <Image src="/icone.svg" alt="Descrição da imagem"  width={100} height={70}/>
-             TECH SOLUTIONS
+      <div className={styles.head}>
+        <div className={styles.heroContent}>
+          {/* Texto */}
+          <section className={styles.texto}>
+            <h1>
+              Soluções de TI para <br /> impulsionar seu negócio
+            </h1>
+            <p>
+              Transforme sua empresa com tecnologia de ponta e suporte <br /> especializado
+            </p>
+            <div className={styles.botoes}>
+              <button className={styles.botao1}>Fale Conosco</button>
+              <button className={styles.botao2}>Nossos Serviços</button>
+            </div>
           </section>
-          <ul className={styles.menubar}>
-        <li>Inicio</li>
-        <li>Serviços</li>
-        <li>Sobre</li>
-        <li>contato</li>
 
-          </ul>
+          {/* Imagem */}
+          <section className={styles.heroImage}>
+            <Image src="/programming-amico.svg" alt="Descrição da imagem" width={700} height={550} />
+          </section>
+        </div>
       </div>
 
-    <div className={styles.head}>
-  <div className={styles.heroContent}>
-    {/* Texto */}
-    <section className={styles.texto}>
-      <h1>Soluções de TI para <br /> impulsionar seu negócio</h1>
-      <p>Transforme sua empresa com tecnologia de ponta e suporte <br /> especializado</p>
-      <div className={styles.botoes}>
-        <button className={styles.botao1}>Fale Conosco</button>
-        <button className={styles.botao2}>Nossos Serviços</button>
-      </div>
-    </section>
-
-    {/* Imagem */}
-    <section className={styles.heroImage}>
-      <Image src="/programming-amico.svg" alt="Descrição da imagem" width={700} height={500} />
-    </section>
-  </div>
-</div>
-</div>
-
-
-
-      
+      {/* Componente Main */}
+      <Main />
+    </div>
   );
 }
---
+++ src/components/main/Main.js
@@ -0,0 +1,23 @@
+import styld from "@/components/main/main.css"
+
+export default function Main(){
+
+    return(
+        <div className="main">
+  <div className="TextoMain">
+      <h2>
+        Nosso Serviços
+      </h2>
+      <p>
+        Oferecemos soluções completas para atender todas as necessidades <br/> de TI da sua empresa.
+      </p>
+  </div>
+  <div className="conteudo">
+      <section>
+        
+      </section>
+  </div>
+    
+</div>
+    );
+}
